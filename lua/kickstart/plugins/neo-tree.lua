42012cd328490fa9663737b8733e66191a2f7747
Nix Genco-Kamin <nix.genco-kamin.ctr@amount.com>
2026-03-10 09:07:50 -0500

    Rails dev

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,8 +1,6 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
----@module 'lazy'
----@type LazySpec
 return {
   'nvim-neo-tree/neo-tree.nvim',
   version = '*',
@@ -15,14 +13,16 @@ return {
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
-  ---@module 'neo-tree'
-  ---@type neotree.Config
   opts = {
     filesystem = {
       window = {
         mappings = {
           ['\\'] = 'close_window',
         },
+      },
+      follow_current_file = {
+        enabled = true,
+        --leave_dirs_open = false,
       },
     },
   },
