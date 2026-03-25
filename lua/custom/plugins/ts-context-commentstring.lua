b734be3fd118ad7d46532a730581f7c90bd63605
Nix Genco-Kamin <nix.genco-kamin.ctr@amount.com>
2026-03-25 15:59:16 -0500

    feat: add treesitter-context and JSX-aware commenting

---
+++ lua/custom/plugins/ts-context-commentstring.lua
@@ -0,0 +1,11 @@
+return {
+  'JoosepAlvworst/nvim-ts-context-commentstring',
+  opts = { enable_autocmd = false },
+  init = function()
+    local get_option = vim.filetype.get_option
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.filetype.get_option = function(filetype, option)
+      return option == 'commentstring' and require('ts_context_commentstring.internal').calculate_commentstring() or get_option(filetype, option)
+    end
+  end,
+}
