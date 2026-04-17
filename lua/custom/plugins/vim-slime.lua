1b7373fb4c17961f9c2f7dbf9b50a9b199b49a1a
Nix Genco-Kamin <nix.genco-kamin.ctr@amount.com>
2026-04-17 12:21:34 -0500

    Fix sending text to rails console

--- lua/custom/plugins/vim-slime.lua
+++ lua/custom/plugins/vim-slime.lua
@@ -11,18 +11,40 @@ local function auto_configure_slime()
   return false
 end
 
+local function slime_send(lines)
+  local min = math.huge
+  for _, l in ipairs(lines) do
+    if l:match '%S' then min = math.min(min, #(l:match '^%s*')) end
+  end
+  if min == math.huge then min = 0 end
+  local dedented = vim.tbl_map(function(l) return l:sub(min + 1) end, lines)
+  vim.fn.chansend(vim.b.slime_config.jobid, table.concat(dedented, '\n') .. '\n')
+end
+
+_G._slime_operator = function(type)
+  local s = vim.api.nvim_buf_get_mark(0, '[')
+  local e = vim.api.nvim_buf_get_mark(0, ']')
+  local lines = vim.api.nvim_buf_get_lines(0, s[1] - 1, e[1], false)
+  if type == 'char' then
+    lines[#lines] = lines[#lines]:sub(1, e[2] + 1)
+    lines[1] = lines[1]:sub(s[2] + 1)
+  end
+  slime_send(lines)
+end
+
 return {
   'jpalardy/vim-slime',
   init = function()
     vim.g.slime_target = 'neovim'
     vim.g.slime_no_mappings = 1
-    vim.g.slime_bracketed_paste = 1
   end,
   keys = {
     {
       'gz',
       function()
-        if auto_configure_slime() then return '<Plug>SlimeMotionSend' end
+        if not auto_configure_slime() then return end
+        vim.o.operatorfunc = 'v:lua._slime_operator'
+        return 'g@'
       end,
       expr = true,
       desc = 'Send motion to terminal',
@@ -30,14 +52,17 @@ return {
     {
       'gzz',
       function()
-        if auto_configure_slime() then vim.cmd 'SlimeSendCurrentLine' end
+        if not auto_configure_slime() then return end
+        slime_send { vim.fn.getline '.' }
       end,
       desc = 'Send line to terminal',
     },
     {
       'gz',
       function()
-        if auto_configure_slime() then vim.cmd "'<,'>SlimeSend" end
+        if not auto_configure_slime() then return end
+        local s, e = vim.fn.line "'<", vim.fn.line "'>"
+        slime_send(vim.api.nvim_buf_get_lines(0, s - 1, e, false))
       end,
       mode = 'x',
       desc = 'Send selection to terminal',
